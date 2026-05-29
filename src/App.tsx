7c3e242b704ffb8d040431f50d7b6f2d564f8a3e
Rahul Vyas <rahulvyas.5100@gmail.com>
2026-05-29 12:22:39 +0530

    adding Header compo

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import "./App.css";
+import Header from "./components/layout/Header";
 
 function App() {
   return (
     <>
+      <Header></Header>
       <h1>Hello, OrgExplorer!</h1>
     </>
   );
